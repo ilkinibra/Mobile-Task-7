f6b659b838b8082a14d31a6397b24aec50eb9cd6
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 15:52:30 +0400

    2.1

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,13 +1,3 @@
-let num =9999;
-
-if(num<100 && num>=10){
-    console.log("iki reqemli ededdir")
-}
-else if(num <10 && num >=1){
-    console.log("bir reqemli ededdir")
-}else if(num <1000 && num >=100){
-    console.log("uc reqemli ededdir")
-}
-else{
-    console.log("max oxuma heddini kecib")
-}
+for(let i=10; i>0;i--){
+    console.log(i)
+  }
