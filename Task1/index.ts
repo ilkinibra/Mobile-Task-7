f61a338c0806c400e314189cbcbc118d524a5e74
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:26:20 +0400

    4.3

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,20 +1,24 @@
-//2. Bir obyekt yaradıb başqa dəyişənə yeni obyekt kimi kopyala.
+//3. 1-dən 10 kimi olan ədədlərin hərfi yazılışın obyektdən istifadə edərək console çıxardın.
 
-const carDimension ={
-    width : 100,
-    height : 130,
-    length : 200,
-    weight : 2000
-};
 
-const carView ={
-    colour : "Black",
-    motor : 5.5,
-    speed : 400
-}
-
-const car ={
-    ...carDimension,
-    ...carView
-}
-console.log(`Colour: ${car.colour}\nMotor: ${car.motor}\nSpeed: ${car.speed}\nWidth: ${car.width}\nHeight: ${car.height}\nLength: ${car.length}\nWeight: ${car.weight}`)
+let numWords = {
+    1: 'bir',
+    2: 'iki',
+    3: 'uc',
+    4: 'dord',
+    5: 'bes',
+    6: 'alti',
+    7: 'yeddi',
+    8: 'sekkiz',
+    9: 'doqquz',
+    10: 'on'
+  };
+  
+  let num = 1;
+  if(num<=10){
+    console.log(numWords[num]);
+  }
+  else{
+    console.log("SERTI KECIR!!!")
+  }
+  
