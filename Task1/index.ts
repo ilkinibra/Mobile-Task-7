3c6a3aa64c74aadf99b81b676b27206bba3acda2
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:12:29 +0400

    3.3

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,12 +1,9 @@
-// 2. Daxil olan parametrin “Tək” və yaxud “Cüt” olduğunu yoxlayıb boolean qaytaran funksiya yazın.
+/// 3. Parametrlərdə “Ad” və “Soyad” qəbul edib aralarında boşluğ ilə qaytaran funksiya yazın.
 
-function isEven (number) {
-    return number % 2 === 0;
-  }
-  
-  function isOdd (number) {
-    return number % 2 !== 0;
-  }
-  
-  console.log(isEven(5)); 
-  console.log(isOdd(5)); 
+const firstName = 'ILKIN';
+const lastName = 'IBRAHIMOV';
+
+function fullName(firstName,lastName){
+    return firstName.concat(' ').concat(lastName);
+}
+console.log(fullName(firstName,lastName));
