10d8d7c216798500ccde59702588d46494400cdb
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:18:41 +0400

    3.6

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,16 +1,10 @@
-//5. Parametrdən daxil olan ədədin faktorialını qaytaran funksiya yaradın.
+// 6. Parametrlərdən gələn intervalda yerləşən ədədləri funksiya ilə console çıxardın. (rekursiya)
 
-function faktorial(n) {
-    if(n===0){
-      return 0;
+function loop(a,b){
+    if(a<b-1){
+        console.log(a+1);
+        a++;
+        loop(a,b);
     }
-      if (n === 1) {
-        return 1;
-      } else {
-        return n * faktorial(n - 1);
-      }
-    }
-    
-    const number = 0;
-    const result = faktorial(number);
-    console.log(`Eded ${number} faktorialı==>${result}`);
+}
+loop(4,8);
