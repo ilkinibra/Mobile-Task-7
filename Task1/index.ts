e9a2cff4fb631f80d4379c8da9a6bd707aa812df
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:31:38 +0400

    4.6

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,8 +1,14 @@
-//5. Hər hansısa bir obyekt yaradıb onun key və value bir sətrdə console çıxardın. Məsələn: Nəticədə “key: name, value: Cavid”.
-const Object1 = { 
-    name: "Ilkin" 
-};
-
-for (const key in Object1) {
-  console.log(`key: ${key}, value: ${Object[key]}`);
+//6. Obyektin hər hansısa bir sahəsinin olub olmadığını yoxluyan bir funksiya yazın.
+const person =
+{
+    Name: "İlkin",
+    Surname: "İbrahimov",
 }
+
+function yoxla(obj,key)
+{
+    return key in obj;
+}
+
+console.log(yoxla(person,"Name"));
+console.log(yoxla(person,"Age"));
