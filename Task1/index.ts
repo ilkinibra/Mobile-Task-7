e00b21b3bc05b902dcb6636901303538a190e945
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:39:19 +0400

    5.6

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,38 +1,18 @@
-//5. Massivdə yerləşən ədələri böyükdən kiçiyə və kiçikdən böyüyə çeşidliyin.
+//6. Massivdən false deyimli (undefined, null, false və s.i.) elementləri silib yeni massiv qaytaran funksiya yazın.
 
-function boyukdenKiciye(arr) {
-    const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = 0; j < n - 1 - i; j++) {
-        if (arr[j] < arr[j + 1]) {
-          const temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
+
+function filterElements(arr) {
+    const filteredArray = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
+        filteredArray.push(arr[i]);
       }
     }
+    return filteredArray;
   }
   
-  function kicikdenBoyuye(arr) {
-    const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-      let minIndex = i;
-      for (let j = i + 1; j < n; j++) {
-        if (arr[j] < arr[minIndex]) {
-          minIndex = j;
-        }
-      }
-      if (minIndex !== i) {
-        const temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-      }
-    }
-  }   
-  const numbers = [52, 2, 9, 1, 5, 6];
-
-  kicikdenBoyuye(numbers);
-  console.log("Kicikden Boyuye", numbers)
+  const originalArray = [1, null, 2, undefined, 3, false, 4, 5];
+  const newArray = filterElements(originalArray);
   
-  boyukdenKiciye(numbers);
-  console.log("BOYUKDEN KICIYE:", numbers);
+  console.log(newArray); 
