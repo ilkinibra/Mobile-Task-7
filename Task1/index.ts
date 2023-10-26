8b6c4823ee972fb9399dceca433c2f5e7f69f945
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:35:25 +0400

    5.2

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,7 +1,11 @@
-//#hometask #js
-//1. Ədədlərdən ibarət olan bir massiv yaradın və onun uzunluğun iki dəfə azaldın.
+//2. Massivdə yerləşən bütün ədədlərin cəmini tapın.
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const halvedArray = numbers.slice(0, Math.ceil(numbers.length / 2));
-console.log(halvedArray);
+let numbers = [1, 2, 3];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+//console.log(numbers[0]);
+console.log("Butun ededler cemi: " + sum);
