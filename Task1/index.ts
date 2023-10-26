2d3bd02faa54e20a4aa1247e176a0e48d5c5f254
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 15:58:17 +0400

    2.4

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,6 +1,15 @@
-let count=0;
-
-for(let i=1; i<=100;i+=2){
-    count++;
-}
-console.log("say : "+count)
+function sademurekkeb(eded){
+    if(eded <=1){
+        return 'ne sade eded nede murekkeb deyil';
+    }
+    for(let i= 2; i<= Math.sqrt(eded);i++){
+        if(eded%i===0){
+            return 'murekkeb eded'
+        }
+    
+    }
+    return 'sade eded'
+    }
+    const eded=31;
+    const statuss =sademurekkeb(eded);
+    console.log(statuss);
