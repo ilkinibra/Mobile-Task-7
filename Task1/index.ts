9b80c3bfcdd5ef1934a30493c2c63add4c78ad39
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 15:45:53 +0400

    1.3

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,5 +1,15 @@
-const V=60;
-const S=180;
+let num1=10;
+let num2=60;
+let num3=30;
 
-const result =((S/V)*60);
-console.log(result);
+if(num1>num2 && num1>num3)
+{
+    console.log(num1)
+}
+else if(num2>num1 && num2>num3)
+{
+    console.log(num2)
+}
+else{
+    console.log(num3)
+}
