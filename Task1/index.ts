3de4cdaf2ea2ddefcd63fd2e42cd27e97188e8e8
ilkinibra <tu7mrkt9v@code.edu.az~>
2023-10-26 16:08:14 +0400

    1.5

--- Task1/index.ts
+++ Task1/index.ts
@@ -1,8 +1,19 @@
-let num = 9;
+const gun = 8; 
 
-if(num % 2 ===0){
-    console.log("ƏDƏD CÜTDÜR!!!")
-}
-else{
-    console.log("ƏDƏD TƏKDİR!!!")
+if (gun === 1) {
+    console.log("Bazar ertesi");
+} else if (gun === 2) {
+    console.log("cerwenbe axwami");
+} else if (gun === 3) {
+    console.log("cerwenbe");
+} else if (gun === 4) {
+    console.log("cume axwami");
+} else if (gun === 5) {
+    console.log("cume");
+} else if (gun === 6) {
+    console.log("wenbe");
+} else if (gun === 7) {
+    console.log("bazar");
+} else {
+    console.log(" heftede yeddi gun var!!!");
 }
